933a8c0a67e1d28aca856b7665a8823257bfd8b2
y-uchi <bonbonheart@gmail.com>
2024-07-02 19:08:07 +0900

    7/2

--- ruby_drill53.rb
+++ ruby_drill53.rb
@@ -19,3 +19,37 @@ month = gets.to_i
 
 days = get_days(year, month)
 p "#{year}年#{month}月は#{days}日間あります"
+
+# 以下リファクタリング例
+# 1
+def get_days(year, month)
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  if month == 2
+    if (year % 4 == 0 && year % 100 != 0) || year % 400 ==0
+      days = 29
+    else
+      days = 28
+    end
+  else
+    days = month_days[month - 1]
+  end
+
+  return days
+end
+
+# 2
+def get_days(year, month)
+  case month
+  when 1, 3, 5, 7, 8, 10, 12
+    days = 31
+  when 4, 6, 9, 11
+    days = 30
+  when 2
+    if (year % 4 == 0 && year % 100 != 0) || year % 400 ==0
+      days = 29
+    else
+      days = 28
+    end
+   end
+  return days
+end
